5ee681d2cae3d767a7bb3f83825b69271a9e5418
Nawaal Syed <202401199@ac-kuwait.edu.kw>
2026-04-19 15:42:01 +0300

    new live link update

--- api/auth.php
+++ api/auth.php
@@ -3,7 +3,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-header('Access-Control-Allow-Origin: https://shadymedows.xo.je');
+header('Access-Control-Allow-Origin: shady-medows-production.up.railway.app');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Allow-Credentials: true');
